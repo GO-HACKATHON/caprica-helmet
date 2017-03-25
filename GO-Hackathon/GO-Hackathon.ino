d1bfd81a73bd218e4df93e8c4c461812f558878e
Fadhil Mochammad <Fadhil Mochammad>
2017-03-25 16:23:44 +0800

    Update alarm

--- GO-Hackathon/GO-Hackathon.ino
+++ GO-Hackathon/GO-Hackathon.ino
@@ -1,16 +1,62 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
+// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//      2013-05-08 - added multiple output formats
+//                 - added seamless Fastwire support
+//      2011-10-07 - initial release
+
+/* ============================================
+  I2Cdev device library code is placed under the MIT license
+  Copyright (c) 2011 Jeff Rowberg
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  ===============================================
+*/
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
 #include "I2Cdev.h"
 #include "MPU6050.h"
 
-#define THRES 12000
+#define THRES 25000
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
 MPU6050 accelgyro;
 
 int16_t ax, ay, az;
+int32_t ax32, ay32, az32;
 int16_t gx, gy, gz;
+int32_t svm, svm_max;
 
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
 #define OUTPUT_READABLE_ACCELGYRO
 
 #define OUTPUT_ACCELGYRO_FALL
@@ -19,37 +65,48 @@ int16_t gx, gy, gz;
 bool blinkState = false;
 
 void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
   Fastwire::setup(400, true);
 #endif
-  Serial.begin(38400);
+
+  // initialize serial communication
+  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+  // it's really up to you depending on your project)
+  Serial.begin(115200);
+
+  // initialize device
   Serial.println("Initializing I2C devices...");
   accelgyro.initialize();
   accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
+  // verify connection
   Serial.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+  // configure Arduino LED for
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // read raw accel/gyro measurements from device
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
+  ax32 = ax; ay32 = ay; az32 = az;
+  svm = ax32 * ax32 + ay32 * ay32 + az32 * az32;
+  svm = sqrt(svm);
 
 #ifdef OUTPUT_READABLE_ACCELGYRO
   // display tab-separated accel/gyro x/y/z values
   Serial.print(ax); Serial.print(",");
   Serial.print(ay); Serial.print(",");
-  Serial.println(az);
+  Serial.print(az); Serial.print(",");
+  Serial.println(svm);
 #endif
 
 #ifdef OUTPUT_ACCELGYRO_FALL
   // display tab-separated accel/gyro x/y/z values
-  if (ax > THRES)
-    Serial.println("FALL");
-  if (az > THRES)
+  if (svm > THRES)
     Serial.println("FALL");
 #endif
 
@@ -59,3 +116,5 @@ void loop() {
   digitalWrite(LED_PIN, blinkState);
   delay(100);
 }
+
+
